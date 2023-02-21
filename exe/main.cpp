e4f818e35ec5d476689927826637068c2dbfc4e8
behnam <behnamn90@icloud.com>
2023-02-21 16:59:21 +0000

    Got rid of comments

--- exe/main.cpp
+++ exe/main.cpp
@@ -67,251 +67,6 @@ void manual_apply_invasion(TransitionManager *localManager, Uni& uni, MyGraph *G
     G->write(movie_file_name);
 }
 
-bool pairCompare(const std::pair<int, int*>& firstElem, const std::pair<int, int*>& secondElem) {
-    return firstElem.first < secondElem.first;
-}
-
-void test_loops(Constants* constants, MyGraph *G,  Design *design){
-    //cout << constants->C_parameter << "\t" << log(constants->C_parameter) << endl;
-    design->staple_pools[0].print_crossovers();
-
-    double Wshape = G->faces_weight();
-    double Gshape = ( (G->numFaces + G->numLong) * log(constants->C_parameter) ) + Wshape;
-    /*
-    for (auto loop = G->Faces.faces.begin(); loop != G->Faces.faces.end(); ++loop){
-        loop->calculate_weight(G->g);
-        W+= loop->weight;
-        Gshape+= log(constants->C_parameter / loop->weight);
-    }*/
-    //G->Faces.print();
-    cout << "Total W = " << Wshape << endl;
-    cout << "Gshape = " << Gshape << endl;
-
-    double Wlast, Glast, Wlocal;
-    for (auto cross = design->staple_pools[0].crossovers.begin();
-         cross != design->staple_pools[0].crossovers.end(); ++cross){
-        std::cout << "--------------------------------------------------------------- adding cr ";
-        std::cout << cross->id << std::endl;
-        Glast = Gshape;
-        G->bind_domain(cross->domains.first);
-        G->bind_domain(cross->domains.second);
-        G->add_crossover(cross);
-        Wshape = G->faces_weight();
-        Gshape = ( (G->numFaces + G->numLong) * log(constants->C_parameter) ) + Wshape;
-        Wlocal = G->shortest_path(cross);
-        cout << "Gshape = " << Gshape << endl;
-        cout << "Delta Gshape = " << Gshape - Glast << endl;
-        cout << "Delta Glocal = " << log(constants->C_parameter / Wlocal) << endl;
-    }
-
-    for (auto cross = design->staple_pools[0].crossovers.begin();
-         cross != design->staple_pools[0].crossovers.end(); ++cross){
-        std::cout << "--------------------------------------------------------------- removing cr ";
-        std::cout << cross->id << std::endl;
-        Wlast = Wshape;
-        Glast = Gshape;
-        G->unbind_domain(cross->domains.first);
-        G->unbind_domain(cross->domains.second);
-        G->remove_crossover(cross);
-        Wshape = G->faces_weight();
-        Gshape = ( (G->numFaces + G->numLong) * log(constants->C_parameter) ) + Wshape;
-        Wlocal = G->shortest_path(cross);
-        cout << "Gshape = " << Gshape << endl;
-        cout << "Delta Gshape = " << Gshape - Glast << endl;
-        cout << "Delta Glocal = " << log(constants->C_parameter / Wlocal) << endl;
-    }
-}
-
-void test_full(Simulation* sim){
-    auto trManager = sim->trManager;
-    MyGraph *G = trManager->G;
-
-    print_embedding_storage(sim->G->g, sim->G->embedding_storage);
-
-    Design* design = trManager->design;
-    StaplePool* pool = &design->staple_pools[0];
-    vector<Transition*> path, reverse_path;
-    for (auto &st: pool->staples){
-        for (auto &trans : st.transitions){
-            if (st.num_domains == 1 && trans->initial_state == State_t(s0) && trans->final_state == State_t(s1))
-                path.push_back(trans);
-            if (st.num_domains == 2 && trans->initial_state == State_t(s00) && trans->final_state == State_t(s01))
-                path.push_back(trans);
-            if (st.num_domains == 3 && trans->initial_state == State_t(s000) && trans->final_state == State_t(s001))
-                path.push_back(trans);
-        }
-    }
-
-    for (auto &st: pool->staples){
-        for (auto &trans : st.transitions){
-            if (st.num_domains == 2 && trans->initial_state == State_t(s01) && trans->final_state == State_t(s11))
-                path.push_back(trans);
-            if (st.num_domains == 3 && st.id <= 0 && trans->initial_state == State_t(s001) && trans->final_state == State_t(s101))
-                path.push_back(trans);
-            if (st.num_domains == 3 && st.id > 0 && trans->initial_state == State_t(s001) && trans->final_state == State_t(s011))
-                path.push_back(trans);
-        }
-    }
-    for (auto &st: pool->staples){
-        for (auto &trans : st.transitions){
-            if (st.num_domains == 3 && st.id > 0 && trans->initial_state == State_t(s011) && trans->final_state == State_t(s111))
-                path.push_back(trans);
-        }
-    }
-    for (auto tr = path.rbegin(); tr!=path.rend(); ++tr){
-        reverse_path.push_back(&(*((*tr)->reverse)));
-    }
-
-    std::cout << "--------------------------------------------------------------- Binding";
-    if (!trManager->local){
-        double Wshape, Gshape;
-        Wshape = G->faces_weight();
-        Gshape = ( (G->numFaces + G->numLong) * log(trManager->constants->C_parameter) ) + Wshape;
-        std::cout << Wshape << "\t" << Gshape;
-    }
-    std::cout << std::endl;
-
-    double k_bi = trManager->inputs->k_bi;
-    double k_uni = trManager->inputs->k_uni;
-    double RT = gas_constant * trManager->ramp->get_T();
-
-    for (auto &tr : path){
-        trManager->reset_possibles();
-        trManager->next = trManager->transitions.begin() + tr->id;
-
-        trManager->set_dG_duplex(trManager->next);
-        trManager->set_dG_stack(trManager->next);
-        trManager->set_dG_shape(trManager->next);
-        //trManager->next->dG_shape = 0;
-
-        if (tr->uni){
-            if (tr->forward){
-                //trManager->set_dG_shape(trManager->next);
-                tr->dG = tr->dG_shape;
-                tr->rate = k_uni * exp( -tr->dG / RT );
-            }
-            else{
-                tr->dG = tr->dG_duplex+tr->dG_stack;
-                tr->rate = k_uni * exp( tr->dG / RT );
-            }
-        }
-        else{
-            if (tr->forward){
-                tr->rate = k_bi * tr->staple->concentration;
-            }
-            else{
-                //if (!trManager->local){trManager->set_dG_shape(trManager->next);}
-                tr->dG = tr->dG_duplex+tr->dG_stack-tr->dG_shape;
-                tr->rate = k_uni * exp( tr->dG / RT );
-            }
-        }
-
-        trManager->total_rate = 100;
-        trManager->tau = 1/trManager->total_rate;
-        trManager->next->tau = trManager->tau;
-
-        std::cout << *trManager->next << "\t";
-        std::cout << trManager->next->dG_duplex << "\t";
-        std::cout << trManager->next->dG_stack << "\t";
-        std::cout << trManager->next->dG_shape << "\t";
-        if (trManager->next->properties.cross)
-            std::cout << trManager->next->crossover.first->type;
-        std::cout << std::endl;
-        trManager->apply_next();
-    }
-
-    std::cout << "--------------------------------------------------------------- Unbinding";
-    if (!trManager->local) {
-        double Wshape, Gshape;
-        Wshape = G->faces_weight();
-        Gshape = ( (G->numFaces + G->numLong) * log(trManager->constants->C_parameter) ) + Wshape;
-        std::cout << Wshape << "\t" << Gshape;
-    }
-    std::cout << std::endl;
-
-
-
-    for (auto &tr : reverse_path){
-        trManager->reset_possibles();
-        trManager->next = trManager->transitions.begin() + tr->id;
-
-        trManager->set_dG_duplex(trManager->next);
-        trManager->set_dG_stack(trManager->next);
-        trManager->set_dG_shape(trManager->next);
-        //trManager->next->dG_shape = 0;
-
-        if (tr->uni){
-            if (tr->forward){
-                //trManager->set_dG_shape(trManager->next);
-                tr->dG = tr->dG_shape;
-                tr->rate = k_uni * exp( -tr->dG / RT );
-            }
-            else{
-                tr->dG = tr->dG_duplex+tr->dG_stack;
-                tr->rate = k_uni * exp( tr->dG / RT );
-            }
-        }
-        else{
-            if (tr->forward){
-                tr->rate = k_bi * tr->staple->concentration;
-            }
-            else{
-                //if (!trManager->local){trManager->set_dG_shape(trManager->next);}
-                tr->dG = tr->dG_duplex+tr->dG_stack-tr->dG_shape;
-                tr->rate = k_uni * exp( tr->dG / RT );
-            }
-        }
-
-        trManager->total_rate = 100;
-        trManager->tau = 1/trManager->total_rate;
-        trManager->next->tau = trManager->tau;
-
-        std::cout << *trManager->next << "\t";
-        std::cout << trManager->next->dG_duplex << "\t";
-        std::cout << trManager->next->dG_stack << "\t";
-        std::cout << trManager->next->dG_shape << "\t";
-        if (trManager->next->properties.cross)
-            std::cout << trManager->next->crossover.first->type;
-        std::cout << std::endl;
-        trManager->apply_next();
-    }
-
-    std::cout << "--------------------------------------------------------------- ";
-    if (!trManager->local) {
-        double Wshape, Gshape;
-        Wshape = G->faces_weight();
-        Gshape = ( (G->numFaces + G->numLong) * log(trManager->constants->C_parameter) ) + Wshape;
-        std::cout << Wshape << "\t" << Gshape;
-    }
-    std::cout << std::endl;
-}
-
-void test_random(){
-    int myidx;
-
-    int inputrandom = 12341;
-    base_generator_type generator(12341);
-    typedef boost::variate_generator<base_generator_type&, uniform_int<> > UniformInt_gen;
-
-    std::vector<int> myvec = {3,5,647,4352,7,4,2};
-    uniform_int<> uniformInt(0, myvec.size()-1);
-    UniformInt_gen uni(generator,uniformInt);
-
-    for (int i = 0; i < 100; i++){
-        myidx = uni();
-        std::cout << i << "\t" << myidx << "\t" << myvec[myidx] << std::endl;
-    }
-
-    std::vector<int> myvec2 = {3,5,647,4352,7,4,2,6,12,65,2,1};
-    uniform_int<> uniformInt2(0, myvec2.size()-1);
-    UniformInt_gen uni2(generator,uniformInt2);
-
-    for (int i = 0; i < 100; i++){
-        myidx = uni2();
-        std::cout << i << "\t" << myidx << "\t" << myvec2[myidx] << std::endl;
-    }
-}
-
 void exact(Simulation* sim){
     ofstream outfile;
     open_trunc(outfile,"Exact.csv","");
@@ -326,8 +81,6 @@ void exact(Simulation* sim){
     outfile << "G_shape";
     outfile << "\n";
 
-    //Constants *constants = sim->constants;
-    //TempRamp *ramp = sim->ramp;
     double T = sim->ramp->get_T();
     double gamma = sim->constants->gamma_parameter;
     double cParameter = sim->constants->C_parameter;
@@ -365,7 +118,6 @@ void exact(Simulation* sim){
             stIdx++;
         }
         MyGraph mygraph(design);
-        //logsumC = mygraph.faces_weight();
         Wshape = mygraph.faces_weight();
         logsumC = ( (mygraph.numFaces + mygraph.numLong) * log(cParameter) ) + Wshape;
         G_shape = -(gas_constant * T * gamma ) * logsumC;
@@ -450,11 +202,9 @@ void Er2_legacy(Simulation* sim){
 
             ST staple = pool->staples.begin()+stIdx;
             staple->initialise_state(state);
-            //design->change_state(pool->staples.begin()+stIdx,state);
             stIdx++;
         }
         MyGraph mygraph(design);
-        //logsumC = mygraph.faces_weight();
         Wshape = mygraph.faces_weight();
         outfile << Wshape << "," << mygraph.numFaces << "," << 0 << std::endl;
         if (i<100) std::cout << pstate << "\t" << mygraph.numFaces << "\t" << mygraph.embedding_storage << std::endl;
